04f11b9cd0338821706b9572b86eb295fc456b9c
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2023-11-14 15:22:27 +0000

    second commit

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,31 @@ struct Node<T>{
     next_node: Option<Box<Node<T>>>,
 }
 impl<'a,T: std::fmt::Display> Node<T>{
-    fn incr(&self) -> Option<& Node<T>>{
-        return Some(&*(self.next_node.as_ref().unwrap()));
+    fn incr(&mut self) -> Option<&mut Node<T>>{
+        return Some(&mut *(self.next_node.as_mut().unwrap()));
     }
     fn print(&self){
         println!("{}",self.value);
+    }
+    fn add_node(&mut self,value: T){
+        let new_node = Node{value,next_node:None};
+        //should transfer ownership
+        self.next_node =  Some(Box::new(new_node));
     }
 }
 
 fn main() {
     let node2 = Node{value:2,next_node:None};
-    let node1 = Node{value:1,next_node:Some(Box::new(node2))};
+    let mut node1 = Node{value:1,next_node:Some(Box::new(node2))};
 
     //try printing out the value of node2 using node 1.
     //first increment node1. his should return an option with a node pointer
     //then unwrap the node ref and try printing
     node1.incr().unwrap().print();
+
+    //should add a 3rd node with the value of 5
+    node1.incr().unwrap().add_node(5);
+    //should print out the new value
+    node1.incr().unwrap().incr().unwrap().print();
+    //i know want to make it easier to mutate these values.
 }
